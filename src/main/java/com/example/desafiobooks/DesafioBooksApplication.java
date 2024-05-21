8de4b5e77b7ccb104c0b2531826e44cd40f6b77f
Merari May Sosa <merarimaysosa@gmail.com>
2024-05-20 21:34:34 -0600

    Creacion del proyecto local

---
+++ src/main/java/com/example/desafiobooks/DesafioBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiobooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBooksApplication.class, args);
+	}
+
+}
